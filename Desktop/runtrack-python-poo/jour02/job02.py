ef168ee0eb7810e13a23d4512971792dac859188
Zenzen <zenbout.l.mohamed@gmail.com>
2024-01-03 10:38:20 +0100

    test sending

---
+++ Desktop/runtrack-python-poo/jour02/job02.py
@@ -0,0 +1,10 @@
+class Rectangle: 
+    def __init__(self, longueur, largeur):
+        self.longueur = longueur
+        self.largeur = largeur
+        
+    def get_longueur(self):
+        return self.longueur
+    
+    def get_largeur(self):
+        return self.largeur
